338cf3278d5513db74a29bb89950a3352fa6916f
Energex22 <energex22@gmail.com>
2026-09-19 15:36:29 -0500

    Make Windows FabOS launcher start packaged application

--- installer/windows7/Start_FabOS.bat
+++ installer/windows7/Start_FabOS.bat
@@ -1,4 +1,24 @@
 @echo off
+setlocal
 cd /d "%~dp0\..\.."
-python -m fabos_desktop.main
-if errorlevel 1 pause
+
+echo Starting FabOS...
+set "APP_EXE=%CD%\dist\WireVault FabOS\WireVault FabOS.exe"
+if not exist "%APP_EXE%" (
+  echo ERROR: Built FabOS executable was not found:
+  echo   %APP_EXE%
+  echo.
+  echo Run installer\windows7\Build_EXE.bat first.
+  pause
+  exit /b 1
+)
+
+start "FabOS" "%APP_EXE%"
+if errorlevel 1 (
+  echo ERROR: Windows could not start FabOS.
+  pause
+  exit /b 1
+)
+
+endlocal
+exit /b 0
