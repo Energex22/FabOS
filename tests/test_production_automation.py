e1b990097e7092a92dd14e70e1f390a9389713de
Energex22 <energex22@gmail.com>
2026-09-22 10:38:47 -0500

    Fix production automation test cleanup for dependent records

--- tests/test_production_automation.py
+++ tests/test_production_automation.py
@@ -24,6 +24,8 @@ class ProductionAutomationTests(unittest.TestCase):
             with app.database.connect() as conn:
                 spool_id = "automation-test-spool"
                 job_id = "automation-test-job"
+                conn.execute("DELETE FROM qc_inspections WHERE print_job_id=?", (job_id,))
+                conn.execute("DELETE FROM manufacturing_observations WHERE print_job_id=?", (job_id,))
                 conn.execute("DELETE FROM print_jobs WHERE id=?", (job_id,))
                 conn.execute("DELETE FROM filament_spools WHERE id=?", (spool_id,))
                 conn.execute("""INSERT INTO filament_spools
