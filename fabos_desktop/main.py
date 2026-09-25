829fca4ec7e7da07b9c6b562a7cd2ac55617a7b2
Energex22 <energex22@gmail.com>
2026-09-25 17:21:43 -0500

    Remove unused marketing UI import

--- fabos_desktop/main.py
+++ fabos_desktop/main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox, ttk, filedialog, simpledialog
+from tkinter import messagebox, ttk, filedialog
 from datetime import datetime
 from pathlib import Path
 import sys
